8249af8ba659c5051608903f691315b704ccb13f
AbdelDer <abdouder97@gmail.com>
2022-10-18 14:53:15 +0100

    ajouter route GET

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const express = require('express');
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.use('/api/todos', (req, res, next) => {
+  const tasks = [
+    {
+      _id: '1',
+      title: 'tache 1',
+      description: 'description de la tache 1'
+    },
+  ];
+  res.status(200).json(tasks);
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Hello world !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
