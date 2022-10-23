c5f79cf9ff95020f45e7b3386ba33106bfad9f09
AbdelDer <abdouder97@gmail.com>
2022-10-23 12:03:57 +0100

    read .env.local file

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const http = require('http');
+const dotenv = require('dotenv');
 const app = require('./app');
+
+dotenv.config({ path: `.env.local`});
+
+console.log(process.env.PORT);
 
 app.set('port', process.env.PORT || 3000);
 const server = http.createServer(app);
