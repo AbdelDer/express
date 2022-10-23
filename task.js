1bd19dc33cecb867e89279d3dc871b9b67884d7e
AbdelDer <abdouder97@gmail.com>
2022-10-23 12:21:11 +0100

    create task schema

---
+++ task.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  userId: { type: String, required: true },
+});
+
+module.exports = mongoose.model('Task', taskSchema);
